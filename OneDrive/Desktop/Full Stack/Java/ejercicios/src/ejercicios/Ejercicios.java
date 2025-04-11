8b4a6357e0697b198eea422bd50beac813e176ca
Adrian Olmedo <adrian.olmedo@hotmail.com>
2025-04-11 15:58:25 -0300

    subiendo Ejercicio java al git

---
+++ OneDrive/Desktop/Full Stack/Java/ejercicios/src/ejercicios/Ejercicios.java
@@ -0,0 +1,33 @@
+//Ejemplo swich version 14 (Como expresion devulve un resultado)
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicios {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("ingrese el dia de la semana");
+
+        String dia = teclado.nextLine();
+        String resultado;
+        resultado = switch (dia) {
+            //  con la combinacion Shift + Control + felcha abajo copio lineas
+            case "lunes", "martes", "miercoles", "jueves", "viernes" ->
+                "Laborable";
+            case "sabado", "dominogo" ->
+                "NO laborable";
+            default -> {  
+                System.out.println("Procesando....");
+                yield "dia no valido";
+//como en el default se habre las llaves de un bloque de codigo q devuelve mas
+//de un mando, es obligatorio el yield en este caso.. (yield=producir)
+//yield NO retun,  usando -> fecha   (return sale del metodo ejm void) 
+            }
+            
+        };      
+            System.out.println(resultado);
+            
+        }
+    }
