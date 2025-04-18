90da5acb7760ccb69af1e8830faf429c280f4521
Adrian Olmedo <adrian.olmedo@hotmail.com>
2025-04-18 16:59:39 -0300

    uso del while pildoras

---
+++ usoWhile.java
@@ -0,0 +1,28 @@
+//uso del bucle while   indeterminado
+/*
+package Pildoras;
+
+import java.util.Scanner;
+
+public class usoWhile {
+
+    public static void main(String[] args) {
+        System.out.println("ingrese el password: ");
+        Scanner teclado = new Scanner(System.in);
+        String clave = "lunes";
+        String pass = " ";
+
+        while (clave.equals(pass) == false) {
+
+            pass = teclado.nextLine();
+
+            if (clave.equals(pass) == false) {
+                System.out.println("ingrese la clave correcta: ");
+            }
+
+        }
+        System.out.println("acceso permitido");
+
+    }
+}
+*/
---
+++ usoWhile2.java
@@ -0,0 +1,22 @@
+//uso del while con JOptionPane v#18 Pildoras
+/*
+package Pildoras;
+import javax.swing.JOptionPane;
+
+public class usoWhile2 {
+
+    public static void main(String[] args) {
+     
+        
+      String clave="elpass";
+      String pass=" ";
+      
+      while(clave.equals(pass)==false){
+          pass=JOptionPane.showInputDialog("ingrese el password:");
+          if (clave.equals(pass)==false)System.out.println("ingrese nuevamente la correcta:");
+      }
+      System.out.println("acceso permitido!!!!");
+    }
+    
+}
+*/
