5854b734554aa915b3ff6675f36385ebccbcc33d
Adrian Olmedo <adrian.olmedo@hotmail.com>
2025-04-24 08:57:40 -0300

    subiendo integrador2

---
+++ Integrador2.java
@@ -0,0 +1,15 @@
+
+package Pildoras;
+
+
+public class Integrador2 {
+    
+    public static void main(String[] args) {
+        int contador=0;
+        
+        do{
+            System.out.println("vuelta numero:  " + contador);
+            contador ++;
+        }while(contador<20);
+    }
+}
